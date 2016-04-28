6a060f2bf8e5717fe1c95d30555d6c6c312d23fd
Mike Gerwitz <gerwitm@lovullo.com>
2016-04-28 15:15:27 -0400

    Add {,Rest}ClientStrategy
    * src/Client/BadClientDataException.php: Added
    * src/Client/ClientStrategy.php: Added
    * src/Client/RestClientStrategy.php: Added
    * test/Client/ClientStrategyTestCase.php: Added
    * test/Client/RestClientStrategyTest.php: Added

---
+++ src/Client/BadClientDataException.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Bad client data exception
+ *
+ *  Copyright (C) 2016 LoVullo Associates, Inc.
+ *
+ *  This file is part of libliza-php.
+ *
+ *  libliza-php is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Lovullo\Liza\Client;
+
+
+/**
+ * Client was provided with data that is incomplete or poorly formatted
+ *
+ * N.B: This exception should not provide the bad data, as that would leak
+ * implementation details (and potentially very sensitive data); this is a
+ * public-facing exception!
+ */
+class BadClientDataException
+    extends \DomainException
+{
+}
